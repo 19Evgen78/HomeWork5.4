bb43e2da0fc408f1d50987e7aafd6feb4e1da0fc
19kuz <19kuzminykh78@mail.ru>
2024-02-29 19:15:44 +0800

    ДЗ 5.4 готова

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            //код который может вызвать исключения
+        } catch (IOException | IllegalArgumentException e){
+        System.out.println("Произошла ошобка :" + e);
+    }catch (Exception e){
+            System.out.println("Ошибка" e);
+        }
+
     }
 }
